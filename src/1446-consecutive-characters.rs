769e9b83a2fe08da0bdc127f2a51748383ce1d86
David Hunt-Mateo <dkmateo@gmail.com>
2020-11-03 13:05:48 -0500

    Add Nov 3rd Daily Challenge - 1446 Consecutive Characters

---
+++ src/1446-consecutive-characters.rs
@@ -0,0 +1,8 @@
+/// Given a string s, the power of the string is the maximum length of a
+/// non-empty substring that contains only one unique character. Return the
+/// power of the string.
+impl Solution {
+    pub fn max_power(s: String) -> i32 {
+
+    }
+}

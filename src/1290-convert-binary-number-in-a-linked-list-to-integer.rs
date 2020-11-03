0bc18fb30153703d8979e011affd78db9ecce82e
David Hunt-Mateo <dkmateo@gmail.com>
2020-11-03 13:05:06 -0500

    Add doc comments

--- src/1290-convert-binary-number-in-a-linked-list-to-integer.rs
+++ src/1290-convert-binary-number-in-a-linked-list-to-integer.rs
@@ -12,6 +12,10 @@ impl ListNode {
     }
 }
 
+/// Given head which is a reference node to a singly-linked list. The value of
+/// each node in the linked list is either 0 or 1. The linked list holds the
+/// binary representation of a number. Return the decimal value of the number in
+/// the linked list.
 impl Solution {
     pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
         let mut decimal = 0;
--- src/147-insertion-sort-list.rs
+++ src/147-insertion-sort-list.rs
@@ -12,6 +12,16 @@ impl ListNode {
     }
 }
 
+/// Sort a linked list using insertion sort.
+///
+/// Algorithm of Insertion Sort:
+///
+/// 1.  Insertion sort iterates, consuming one input element each repetition,
+///     and growing a sorted output list.
+/// 2.  At each iteration, insertion sort removes one element from the input
+///     data, finds the location it belongs within the sorted list, and inserts
+///     it there.
+/// 3.  It repeats until no input elements remain.
 impl Solution {
     pub fn insertion_sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         match head {

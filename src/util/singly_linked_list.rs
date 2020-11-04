91ac982bae171aa51b44109744d7dba1e6445071
David Hunt-Mateo <dkmateo@gmail.com>
2020-11-04 18:47:44 -0500

    Add function and macro to convert vector to singly-linked list, for testing

--- src/util/singly_linked_list.rs
+++ src/util/singly_linked_list.rs
@@ -11,3 +11,20 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+
+// helper function for test
+pub fn to_singly_linked_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut current = None;
+    for &v in vec.iter().rev() {
+        let mut node = ListNode::new(v);
+        node.next = current;
+        current = Some(Box::new(node));
+    }
+    current
+}
+
+#[macro_export]
+macro_rules! sll {
+    ($($e:expr),*) => {to_singly_linked_list(vec![$($e.to_owned()), *])};
+    ($($e:expr,)*) => {to_singly_linked_list(vec![$($e.to_owned()), *])};
+}

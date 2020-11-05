4137cb5f0442cc08aeb023a8a64cede63869b28a
David Hunt-Mateo <dkmateo@gmail.com>
2020-11-05 12:12:35 -0500

    Add prompt for #1217 Minimum Cost To Move Chips to the Same Position

---
+++ src/lc1217_minimum_cost_to_move_chips_to_the_same_position.rs
@@ -0,0 +1,40 @@
+/// We have n chips, where the position of the ith chip is position[i].
+///
+/// We need to move all the chips to the same position. In one step, we can
+/// change the position of the ith chip from position[i] to:
+///
+/// * position[i] + 2 or position[i] - 2 with cost = 0.
+/// * position[i] + 1 or position[i] - 1 with cost = 1.
+///
+/// Return the minimum cost needed to move all the chips to the same position.
+///
+/// Constraints:
+///
+/// * 1 <= position.length <= 100
+/// * 1 <= position[i] <= 10^9
+pub struct Solution;
+impl Solution {
+    pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn uniform_distribution() {
+        assert_eq!(1, Solution::min_cost_to_move_chips(vec![1, 2, 3]));
+    }
+
+    #[test]
+    fn center_bias() {
+        assert_eq!(2, Solution::min_cost_to_move_chips(vec![2, 2, 2, 3, 3]));
+    }
+
+    #[test]
+    fn large_split() {
+        assert_eq!(1, Solution::min_cost_to_move_chips(vec![1, 1000000000]));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod lc0147_insertion_sort_list;
 mod lc0310_minimum_height_trees;
+mod lc1217_minimum_cost_to_move_chips_to_the_same_position;
 mod lc1290_convert_binary_number_in_a_linked_list_to_integer;
 mod lc1446_consecutive_characters;
 mod util;

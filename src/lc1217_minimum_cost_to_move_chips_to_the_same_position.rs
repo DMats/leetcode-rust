5cc19ace7c7891b69ddbddfdc4dd85851a856a12
David Hunt-Mateo <dkmateo@gmail.com>
2020-11-05 17:47:01 -0500

    Add debug logging

--- src/lc1217_minimum_cost_to_move_chips_to_the_same_position.rs
+++ src/lc1217_minimum_cost_to_move_chips_to_the_same_position.rs
@@ -28,20 +28,31 @@ impl Solution {
     #[allow(unused_variables)]
     pub fn min_cost_to_move_chips(position: Vec<i32>) -> i32 {
         let mut min_cost = std::i32::MAX;
+        dbg!(min_cost);
         for (destination, _) in position.iter().enumerate() {
+            dbg!(destination);
             let mut total_cost = 0;
+            dbg!(total_cost);
             for (current_location, chip_count) in position.iter().enumerate() {
+                dbg!(current_location);
+                dbg!(chip_count);
                 let (distance_to_destination, overflow_occurred) =
                     current_location.overflowing_sub(destination);
+                dbg!(distance_to_destination);
+                dbg!(overflow_occurred);
                 let cost_to_move_one_chip = (match overflow_occurred {
                     // If overflow occurred, reverse the subtraction
                     true => destination - current_location,
                     false => distance_to_destination,
                 } % 2) as i32;
+                dbg!(cost_to_move_one_chip);
                 let cost_to_move_multiple_chips = chip_count * cost_to_move_one_chip;
+                dbg!(cost_to_move_multiple_chips);
                 total_cost += cost_to_move_multiple_chips;
+                dbg!(total_cost);
             }
             min_cost = std::cmp::min(min_cost, total_cost);
+            dbg!(min_cost);
         }
         min_cost
     }

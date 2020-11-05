1e2a1216ff8baf4535b7c8c6db94cd76371cfbcd
David Hunt-Mateo <dkmateo@gmail.com>
2020-11-05 12:24:33 -0500

    Reorder mod statements in crate root (lib.rs)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 // Disable dead code warnings LeetCode solutions are intentionally not used
 #![allow(dead_code)]
 
+mod util;
+
 mod lc0147_insertion_sort_list;
 mod lc0310_minimum_height_trees;
 mod lc1217_minimum_cost_to_move_chips_to_the_same_position;
 mod lc1290_convert_binary_number_in_a_linked_list_to_integer;
 mod lc1446_consecutive_characters;
-mod util;

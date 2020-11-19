794e0de4822b02b16e8a4e3177a9affeb3802148
David Hunt-Mateo <dhunt-mateo@tamrd.com>
2020-11-19 17:17:00 -0500

    Add Apache 2.0 license headers to each source file, enforce with rustfmt

--- src/lc1283_find_the_smallest_divisor_given_a_threshold.rs
+++ src/lc1283_find_the_smallest_divisor_given_a_threshold.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 David Hunt-Mateo (DMats)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /// Given an array of integers `nums` and an integer `threshold`, we will choose
 /// a positive integer divisor and divide all the array by it and sum the result
 /// of the division. Find the **smallest** divisor such that the result
